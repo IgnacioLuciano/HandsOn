d369fdba0e4d0bc91486e3b9e7ce5a2eec4eddde
Unknown <iglumu@gmail.com>
2017-12-19 12:30:47 -0800

    Ex2

--- index.js
+++ index.js
@@ -56,3 +56,16 @@ this.boat = boat;
 };
 console.log(wishlist("Renault" + "Red" + "20,000"));
 
+
+
+function phoneNumberFormat(phoneNumber) {
+  phoneNumber = document.getElementById("phNumber").value;
+  let phoneRegex = /^\(\d\d\d\) \d\d\d-\d\d\d\d$/g;
+  if (phoneNumber.match(phoneRegex)) {
+    alert("Valid number entered");
+  } else {
+    alert("Phone number entered is invalid");
+  }
+}
+
+
